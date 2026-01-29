1543fc19d4c80d94e97d8cb19a85a2091f8af459
Anandhan_R <anandhanr668@gmail.com>
2026-01-29 10:47:41 +0530

    Initialze of Frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+/*import Login from "./login.jsx";
+
+function App() {
+  return <Login />;
+}
+
+export default App;
+*/
+import FormBuilder from "./FormBuilder";
+
+function App() {
+  return <FormBuilder />;
+}
+
+export default App;
